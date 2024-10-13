ad6063ec5829f4f570a5d30a19652e477fd3fce2
Wilson Ibekason <102219342+wilsonibekason@users.noreply.github.com>
2024-10-13 10:48:49 +0100

    wroking on clerk configuration

--- tokenserve-web-app/app/layout.tsx
+++ tokenserve-web-app/app/layout.tsx
@@ -2,12 +2,28 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Script from "next/script";
+import { Metadata } from "next";
+import localFont from "next/font/local";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
+  metadataBase: new URL("https://clerk-next-app.vercel.app/"),
   title: "TokenServe",
   description: "A skill-based volunteering platform with token rewards",
+  openGraph: { images: ["/og.png"] },
 };
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +31,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#000000" },
+        elements: {
+          formButtonPrimary:
+            "bg-black border border-black border-solid hover:bg-white hover:text-black",
+          socialButtonsBlockButton:
+            "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+          socialButtonsBlockButtonText: "font-semibold",
+          formButtonReset:
+            "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+          membersPageInviteButton:
+            "bg-black border border-black border-solid hover:bg-white hover:text-black",
+          card: "bg-[#fafafa]",
+        },
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
--- tokenserve-web-app/middleware.ts
+++ tokenserve-web-app/middleware.ts
@@ -1,20 +1,15 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// export default authMiddleware({
-//   publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY, // Fetch the key from environment variables
-//   secretKey: process.env.CLERK_SECRET_KEY,
-// });
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs";
-
-export default clerkMiddleware({
-  // Any options or custom logic you need can be placed here
+export default clerkMiddleware((auth, req) => {
+  console.log(
+    "Publishable Key:",
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+  );
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
